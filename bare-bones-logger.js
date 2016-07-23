795f2494f721c410cdc74f5170682509df541a7a
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-07-23 08:16:34 -0400

    formatting

--- bare-bones-logger.js
+++ bare-bones-logger.js
@@ -1,8 +1,4 @@
-"use strict";
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
-
-var logger = function () {
+var logger = (function () {
   "use strict";
 
   var send = null;
@@ -10,9 +6,9 @@ var logger = function () {
   function cloneArgs(args) {
     var output = [];
     for (var i = 0; i < args.length; ++i) {
-      if (_typeof(args[i]) === "object" && !(args[i] instanceof String)) {
+      if (typeof args[i] === "object" && !(args[i] instanceof String)) {
         var obj1 = args[i],
-            obj2 = {};
+          obj2 = {};
         for (var key in obj1) {
           obj2[key] = obj1[key];
           if (obj2[key] !== null && obj2[key] !== undefined) {
@@ -20,12 +16,14 @@ var logger = function () {
           }
         }
         output.push(obj2);
-      } else {
+      }
+      else {
         output.push(args[i].toString());
       }
     }
     return output;
   }
+
 
   function wrap(name) {
     var orig = name;
@@ -49,35 +47,40 @@ var logger = function () {
     WEBSOCKET: 2,
     DOM: 3,
     USER: 4
-  };
+  }
 
   logger.setup = function setup(type, target) {
     if (type !== logger.DISABLED) {
       if ((type === logger.HTTP || type === logger.WEBSOCKET) && location.protocol === "file:") {
         console.warn("Can't perform HTTP requests from the file system. Not going to setup the error proxy, but will setup the error catch-all.");
-      } else if (type === logger.HTTP) {
-        send = function send(data) {
+      }
+      else if (type === logger.HTTP) {
+        send = function (data) {
           var req = new XMLHttpRequest();
           req.open("POST", target);
           req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
           req.send(data);
         };
-      } else if (type === logger.WEBSOCKET) {
+      }
+      else if (type === logger.WEBSOCKET) {
         var socket = new WebSocket(target);
-        send = function send(data) {
+        send = function (data) {
           socket.send(data);
         };
-      } else if (type === logger.DOM) {
+      }
+      else if (type === logger.DOM) {
         var output = document.querySelector(target);
-        send = function send(data) {
+        send = function (data) {
           var elem = document.createElement("pre");
           elem.appendChild(document.createTextNode(data));
           output.appendChild(elem);
         };
-      } else if (type === logger.USER) {
+      }
+      else if (type === logger.USER) {
         if (!(target instanceof Function)) {
           console.warn("The target parameter was expected to be a function, but it was", target);
-        } else {
+        }
+        else {
           send = target;
         }
       }
@@ -90,16 +93,17 @@ var logger = function () {
 
       window.onerror = function (message, source, lineno, colno, error) {
         console.error(JSON.stringify({
-          time: new Date().toLocaleTimeString(),
+          time: (new Date())
+            .toLocaleTimeString(),
           message: message,
           source: source,
           lineno: lineno,
           colno: colno,
           error: error.message
         }));
-      };
+      }
     }
   };
 
   return logger;
-}();
+})();
