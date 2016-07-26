951f57fc4f2064e732177e018da79ff40eef54ff
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-07-26 13:35:27 -0400

    updated build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 ﻿var gulp = require("gulp"),
-  nt = require("../notiontheory-basic-build/src/index.js").setup(gulp, true),
+  nt = require("notiontheory-basic-build").setup(gulp, true),
   tasks = nt.js(
     "bare-bones-logger",
     ["src/**/*.js"]);
