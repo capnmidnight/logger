0a39d2e224e59205c9fda944010135a252f63f11
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-10-17 19:26:03 -0400

    dev build without minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 ﻿var gulp = require("gulp"),
   pkg = require("./package.json"),
-  nt = require("notiontheory-basic-build").setup(gulp, pkg),
-  js = nt.js("bare-bones-logger", "src", ["format"]);
+  nt = require("../notiontheory-basic-build/src").setup(gulp, pkg),
+  js = nt.js("bare-bones-logger", "src", ["format"]),
+  jsDev = nt.js("bare-bones-logger", "src", ["format"], null, true);
 
 gulp.task("format", [js.format]);
-gulp.task("default", [js.default]);
+gulp.task("default", [jsDev.default]);
 gulp.task("release", [js.build]);
